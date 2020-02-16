14bda65acd40c076c34e941cb77f5f6837b5c18d
alex <alex@SERVER>
2020-02-16 13:26:31 +0300

    fix update

--- update.sh
+++ update.sh
@@ -6,4 +6,4 @@ DataBase="$GufenDir/streams.db"
 OutputJS="$GufenDir/assets/js/data.js"
 ServiceToken="533bacf01e11f55b536a565b57531ac114461ae8736d6506a3"
 
-/usr/bin/python3 "$GufenDir/gufenator.py -t $ServiceToken -d $DataBase -j $OutputJS"
+/usr/bin/python3 "$GufenDir/gufenator.py" -t "$ServiceToken" -d "$DataBase" -j "$OutputJS"
